45d45f66b39a2f5b1ea2f2706270eae4f7a3ac8c
sallecta <sallecta@inbox.ru>
2019-03-10 14:04:56 +0300

    ...

--- gitpush.sh
+++ gitpush.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+echo Enter commit message:
+read commmitMessage
+if [ "$commmitMessage" = "" ]
+then
+    commmitMessage='...'
+fi
+echo commit message is "\$commmitMessage"\
+exit
 git add *
-git commit --message=... 
+git commit --message=$commmitMessage
 git push
